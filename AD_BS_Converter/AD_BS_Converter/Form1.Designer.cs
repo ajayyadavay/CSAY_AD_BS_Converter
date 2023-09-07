33e5834557b742d4f53ba8157bbde0f0e60d2b5f
ajayyadavay <ajayyadavay02@gmail.com>
2023-09-07 14:59:14 +0545

    First commits

---
+++ AD_BS_Converter/AD_BS_Converter/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace AD_BS_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtBaseDateAD = new System.Windows.Forms.TextBox();
+            this.TxtBaseDateBS = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtNewDateAD = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtNewDateBS = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TxtN_Days = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TxtDayslistbox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.TxtMonthsinListbox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.BtnADtoBS = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.TxtTotalDays = new System.Windows.Forms.TextBox();
+            this.LblRemarks = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(45, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Base date (AD)";
+            // 
+            // TxtBaseDateAD
+            // 
+            this.TxtBaseDateAD.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtBaseDateAD.Location = new System.Drawing.Point(161, 54);
+            this.TxtBaseDateAD.Name = "TxtBaseDateAD";
+            this.TxtBaseDateAD.Size = new System.Drawing.Size(196, 22);
+            this.TxtBaseDateAD.TabIndex = 1;
+            this.TxtBaseDateAD.Text = "2022-04-14";
+            this.TxtBaseDateAD.TextChanged += new System.EventHandler(this.TxtBaseDateAD_TextChanged);
+            // 
+            // TxtBaseDateBS
+            // 
+            this.TxtBaseDateBS.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtBaseDateBS.Location = new System.Drawing.Point(492, 54);
+            this.TxtBaseDateBS.Name = "TxtBaseDateBS";
+            this.TxtBaseDateBS.Size = new System.Drawing.Size(196, 22);
+            this.TxtBaseDateBS.TabIndex = 3;
+            this.TxtBaseDateBS.Text = "2079-01-01";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(381, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Base date (BS)";
+            // 
+            // TxtNewDateAD
+            // 
+            this.TxtNewDateAD.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtNewDateAD.Location = new System.Drawing.Point(161, 93);
+            this.TxtNewDateAD.Name = "TxtNewDateAD";
+            this.TxtNewDateAD.Size = new System.Drawing.Size(196, 22);
+            this.TxtNewDateAD.TabIndex = 5;
+            this.TxtNewDateAD.TextChanged += new System.EventHandler(this.TxtNewDateAD_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(50, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "New date (AD)";
+            // 
+            // TxtNewDateBS
+            // 
+            this.TxtNewDateBS.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtNewDateBS.Location = new System.Drawing.Point(492, 96);
+            this.TxtNewDateBS.Name = "TxtNewDateBS";
+            this.TxtNewDateBS.Size = new System.Drawing.Size(196, 22);
+            this.TxtNewDateBS.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(381, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "New date (BS)";
+            // 
+            // TxtN_Days
+            // 
+            this.TxtN_Days.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtN_Days.Location = new System.Drawing.Point(241, 133);
+            this.TxtN_Days.Name = "TxtN_Days";
+            this.TxtN_Days.Size = new System.Drawing.Size(116, 22);
+            this.TxtN_Days.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(50, 136);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(185, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "No. of days (New - Base date)";
+            // 
+            // TxtDayslistbox
+            // 
+            this.TxtDayslistbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtDayslistbox.Location = new System.Drawing.Point(389, 170);
+            this.TxtDayslistbox.Multiline = true;
+            this.TxtDayslistbox.Name = "TxtDayslistbox";
+            this.TxtDayslistbox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TxtDayslistbox.Size = new System.Drawing.Size(299, 128);
+            this.TxtDayslistbox.TabIndex = 10;
+            this.TxtDayslistbox.Text = "31,31,32,31,31,31,30,29,30,29,30,30,31,32,31,32,31,30";
+            this.TxtDayslistbox.TextChanged += new System.EventHandler(this.TxtDayslistbox_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(386, 151);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(155, 16);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "No. of days in BS months";
+            // 
+            // TxtMonthsinListbox
+            // 
+            this.TxtMonthsinListbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtMonthsinListbox.Location = new System.Drawing.Point(574, 301);
+            this.TxtMonthsinListbox.Name = "TxtMonthsinListbox";
+            this.TxtMonthsinListbox.Size = new System.Drawing.Size(68, 22);
+            this.TxtMonthsinListbox.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(386, 301);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(162, 16);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "No. of Months in above list";
+            // 
+            // BtnADtoBS
+            // 
+            this.BtnADtoBS.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnADtoBS.Location = new System.Drawing.Point(161, 253);
+            this.BtnADtoBS.Name = "BtnADtoBS";
+            this.BtnADtoBS.Size = new System.Drawing.Size(182, 45);
+            this.BtnADtoBS.TabIndex = 14;
+            this.BtnADtoBS.Text = "Convert AD to BS";
+            this.BtnADtoBS.UseVisualStyleBackColor = true;
+            this.BtnADtoBS.Click += new System.EventHandler(this.BtnADtoBS_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(386, 329);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(182, 16);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Total no. of Days in above list";
+            // 
+            // TxtTotalDays
+            // 
+            this.TxtTotalDays.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtTotalDays.Location = new System.Drawing.Point(574, 329);
+            this.TxtTotalDays.Name = "TxtTotalDays";
+            this.TxtTotalDays.Size = new System.Drawing.Size(68, 22);
+            this.TxtTotalDays.TabIndex = 15;
+            // 
+            // LblRemarks
+            // 
+            this.LblRemarks.AutoSize = true;
+            this.LblRemarks.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblRemarks.Location = new System.Drawing.Point(50, 173);
+            this.LblRemarks.Name = "LblRemarks";
+            this.LblRemarks.Size = new System.Drawing.Size(65, 16);
+            this.LblRemarks.TabIndex = 17;
+            this.LblRemarks.Text = "Remarks:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(781, 485);
+            this.Controls.Add(this.LblRemarks);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.TxtTotalDays);
+            this.Controls.Add(this.BtnADtoBS);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.TxtMonthsinListbox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.TxtDayslistbox);
+            this.Controls.Add(this.TxtN_Days);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.TxtNewDateBS);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TxtNewDateAD);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TxtBaseDateBS);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtBaseDateAD);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "CSAY AD to BS Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtBaseDateAD;
+        private System.Windows.Forms.TextBox TxtBaseDateBS;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtNewDateAD;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtNewDateBS;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TxtN_Days;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox TxtDayslistbox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox TxtMonthsinListbox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button BtnADtoBS;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox TxtTotalDays;
+        private System.Windows.Forms.Label LblRemarks;
+    }
+}
+
---
+++ AD_BS_Converter/AD_BS_Converter/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace AD_BS_Converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Find_No_of_Months_BS();
+            SumofDaysinList();
+        }
+
+        private void Remarks()
+        {
+            int n_days, totaldays;
+            n_days = Convert.ToInt32(TxtN_Days.Text);
+            totaldays = Convert.ToInt32(TxtTotalDays.Text);
+
+            if(n_days >=0 && n_days <= totaldays)
+            {
+                LblRemarks.Text = "OK. Click convert AD to BS";
+                LblRemarks.ForeColor = Color.ForestGreen;
+            }
+            else
+            {
+                LblRemarks.Text = "Not OK. New AD date is out of range";
+                LblRemarks.ForeColor = Color.Red;
+            }
+        }
+
+        private void SumofDaysinList()
+        {
+            try
+            {
+                int n_mnths_DB = Convert.ToInt32(TxtMonthsinListbox.Text);
+                string[] days_of_Month_BS = TxtDayslistbox.Text.Split(',');
+                int[] IntDays = new int[n_mnths_DB];
+
+                int sum = 0;
+
+                for (int i = 0; i < n_mnths_DB; i++)
+                {
+                    IntDays[i] = Convert.ToInt32(days_of_Month_BS[i]);
+                    sum += IntDays[i];
+                }
+
+                TxtTotalDays.Text = sum.ToString();
+            }
+            catch
+            {
+
+            }
+            
+        }
+
+        private void Find_No_of_Months_BS()
+        {
+            try
+            {
+                int len = TxtDayslistbox.Text.Split(',').Length;
+                TxtMonthsinListbox.Text = len.ToString();
+            }
+            catch
+            {
+
+            }
+
+        }
+
+        private void Find_No_of_days_AD()
+        {
+            try
+            {
+                int n_days = DifferenceInDate(TxtBaseDateAD.Text, TxtNewDateAD.Text);
+                TxtN_Days.Text = n_days.ToString();
+            }
+            catch
+            {
+
+            }
+            
+        }
+
+        private int DifferenceInDate(string StartDate, string EndDate)
+        {
+            try
+            {
+                //Date should be in format YYYY-MM-DD e.g. 2022-02-23
+                int year1, month1, days1, year2, month2, days2;
+                string[] temp_date1 = StartDate.Split('-');
+                string[] temp_date2 = EndDate.Split('-');
+
+                /*int[] monthdays = new int[]
+                {
+                    31,28,31,30,31,30,31,31,30,31,30,31
+                };*/
+
+                year1 = Convert.ToInt32(temp_date1[0]);
+                month1 = Convert.ToInt32(temp_date1[1]);
+                days1 = Convert.ToInt32(temp_date1[2]);
+
+                year2 = Convert.ToInt32(temp_date2[0]);
+                month2 = Convert.ToInt32(temp_date2[1]);
+                days2 = Convert.ToInt32(temp_date2[2]);
+
+                DateTime start = new DateTime(year1, month1, days1);
+                DateTime end = new DateTime(year2, month2, days2);
+
+                TimeSpan difference = end - start; //create TimeSpan object
+
+                return difference.Days;
+
+
+            }
+            catch
+            {
+                return 0;
+            }
+
+        }
+
+        private void TxtBaseDateAD_TextChanged(object sender, EventArgs e)
+        {
+            Find_No_of_days_AD();
+        }
+
+        private void TxtNewDateAD_TextChanged(object sender, EventArgs e)
+        {
+            Find_No_of_days_AD();
+            Remarks();
+        }
+
+        private void TxtDayslistbox_TextChanged(object sender, EventArgs e)
+        {
+            Find_No_of_Months_BS();
+            SumofDaysinList();
+        }
+
+        private void BtnADtoBS_Click(object sender, EventArgs e)
+        {
+            int year1, month1, days1, year2=0, month2=0, days2=0;
+            string[] temp_date1 = TxtBaseDateBS.Text.Split('-');
+            string[] temp_date2 = TxtNewDateBS.Text.Split('-');
+
+            /*int[] monthdays = new int[]
+            {
+                31,28,31,30,31,30,31,31,30,31,30,31
+            };*/
+
+            year1 = Convert.ToInt32(temp_date1[0]);
+            month1 = Convert.ToInt32(temp_date1[1]);
+            days1 = Convert.ToInt32(temp_date1[2]);
+
+            
+
+            int n_mnths_DB = Convert.ToInt32(TxtMonthsinListbox.Text);
+            string[] days_of_Month_BS = TxtDayslistbox.Text.Split(',');
+            int[] IntDays = new int[n_mnths_DB];
+
+            for(int i = 0; i < n_mnths_DB; i++)
+            {
+                IntDays[i] = Convert.ToInt32(days_of_Month_BS[i]);
+            }
+
+            int rem_days = 0;
+            rem_days = Convert.ToInt32(TxtN_Days.Text);
+            for (int i = 0; i < n_mnths_DB; i++)
+            {
+                if (IntDays[i] <= rem_days)
+                {
+                    year2 = year1;
+                    month2 = month1 + 1;
+                    days2 = days1;
+
+                    if(month2 > 12)
+                    {
+                        year2++;
+                        month2 -= 12;
+                    }
+
+                    rem_days -= IntDays[i];
+                    year1 = year2;
+                    month1 = month2;
+                    days1 = days2;
+                }
+                else if (IntDays[i] > rem_days)
+                {
+                    year2 = year1;
+                    month2 = month1;
+                    days2 = days1 + rem_days;
+
+                    if (month2 > 12)
+                    {
+                        year2++;
+                        month2 -= 12;
+                    }
+
+                    rem_days = 0;
+                    break;
+                }
+            }
+
+            TxtNewDateBS.Text = year2.ToString() + "-" + month2.ToString() + "-" + days2.ToString();
+        }
+    }
+}
